d4820cb76294fe07820ae44833ca4415935d1edd
Pauliusstep <steponkevicius.paulius@gmail.com>
2022-09-10 18:18:31 +0300

    9 task done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,27 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive () {
+        if (this.budget >= 100000000) {
+            return `The film "${this.title}", directed by ${this.director} has a ${this.budget} USD budget`;
+        } else {
+            return `Sorry, the film "${this.title}" budget too small)`      
+        }
+    }
+};
+
+const CasinoRoyale = new Movie('Casino Royale', 'Martin Campbell', 102000000);
+const Avatar = new Movie ('Avatar', 'James Cameron', 237000000);
+const ecoBudgetMovie = new Movie ('Eco', 'Petras', 50);
+
+console.log(CasinoRoyale.wasExpensive());
+console.log(Avatar.wasExpensive());
+console.log(ecoBudgetMovie.wasExpensive());
